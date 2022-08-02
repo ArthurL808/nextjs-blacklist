d743e89ca013a7fa1062b42eff92a5695f7d5ea4
ArthurL808 <arthurleedev@gmail.com>
2022-08-01 17:16:50 -1000

    made small css changes and added a search input

--- pages/defendantList.js
+++ pages/defendantList.js
@@ -1,8 +1,20 @@
 import defendants from '../defendants.json'
-const DefendantList = () =>{
+import Styles from '../styles/DefendantsList.module.scss'
+import {useState} from 'react'
 
-    return <table>
-        <thead>
+const DefendantList = () =>{
+    const [defendantSearch, setDefendantSearch] = useState('')
+
+    return <div className={Styles.DefendantsListContainer}>
+
+        <label htmlFor="defendantSearch">Defendant Search: </label>
+        <input name='defendantSearch' type="text" placeholder='Search Name' onChange={(e)=>{
+            setDefendantSearch(e.target.value)
+        }}></input>
+
+
+    <table className={Styles.defendantList}>
+        <thead className={Styles.defendantListHeader}>
         <tr>
             <th>First Name</th>
             <th>Last Name</th>
@@ -14,14 +26,14 @@ const DefendantList = () =>{
             <th>Blacklist Reason</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody className={Styles.defendantListBody}>
         {defendants.map((defendant)=>{
-           return( <tr key={defendant.id}>
+            return( <tr className={Styles.defendantRow} key={defendant.id}>
                 <td>{defendant.first_name}</td>
                 <td>{defendant.last_name}</td>
                 <td>{defendant.dob}</td>
-                <td>{defendant.height}</td>
-                <td>{defendant.weight}</td>
+                <td>{defendant.height} Inches</td>
+                <td>{defendant.weight} Lbs</td>
                 <td>{defendant.gender}</td>
                 <td>{defendant.race}</td>
                 <td>{defendant.reasons}</td>
@@ -29,6 +41,7 @@ const DefendantList = () =>{
         })}
         </tbody>
     </table>
+        </div>
 }
 
 export default DefendantList
