08dbad51499c02081f4131b20bd4b4b3f241ae78
ArthurL808 <arthurleedev@gmail.com>
2022-08-29 11:13:14 -1000

    fixes UI bug where shows Incorrect login status

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session, status} = useSession()
+
+  if(status === 'loading') {
+    return null
+  }
   if (session) {
     return (
       <>
